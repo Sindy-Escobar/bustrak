dcd4de2fd7dde73f8f7ba1fc2e22e00f7371f2f9
RobertoAyes <roberto.ayestas@unah.hn>
2025-10-19 10:30:31 -0600

    Actualicé rutas del módulo empleados

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
+Route::resource('empleados', EmpleadoController::class);
+
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
