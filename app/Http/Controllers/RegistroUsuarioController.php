ce4d4cfc10999e163f0b3d0d631e1c103d805bff
SindyEscobar <Sescobarc@unah.hn>
2025-10-20 16:05:38 -0600

    visualizacion de usuarios y detalles, creacion de tabla de registro de usuario

--- app/Http/Controllers/RegistroUsuarioController.php
+++ app/Http/Controllers/RegistroUsuarioController.php
@@ -4,15 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Models\usuario;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegistroUsuarioController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $search = $request->input('search');
+
+        $usuarios = Usuario::query()
+            ->when($search, function ($query, $search) {
+                $query->where(function ($q) use ($search) {
+                    $q->where('nombre_completo', 'like', "%{$search}%")
+                        ->orWhere('email', 'like', "%{$search}%")
+                        ->orWhere('dni', 'like', "%{$search}%");
+                });
+            })
+            ->paginate(10);
+
+        return view('usuarios.index', compact('usuarios'));
     }
 
     /**
@@ -20,7 +33,7 @@ class RegistroUsuarioController extends Controller
      */
     public function create()
     {
-        //
+     return view ('usuarios.create');
     }
 
     /**
@@ -28,16 +41,26 @@ class RegistroUsuarioController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
+        request()->validate([
             'nombre_completo' => 'required|string|max:255',
             'dni' => 'required|string|unique:usuarios,dni',
             'email' => 'required|email|unique:usuarios,email',
             'telefono' => 'required|string|max:20',
             'password' => 'required|string|min:8|confirmed',
         ]);
+        $nuevoUsuario = new Usuario();
 
-        usuario::create($request->all());
-        return redirect('/registro')->with('success', 'Registro exitoso.');
+        $nuevoUsuario -> nombre_completo = request('nombre_completo');
+        $nuevoUsuario -> dni = request('dni');
+        $nuevoUsuario ->email = request('email');
+        $nuevoUsuario -> telefono  = request('telefono');
+        $nuevoUsuario -> password = Hash::make(request('password'));
+
+        if($nuevoUsuario -> save()){
+            return redirect()->route('usuarios.index')->with('success', 'El usuario se ha registrado exitosamente');
+        }else{
+            //No pudo guardar
+        }
     }
 
     /**
@@ -45,7 +68,8 @@ class RegistroUsuarioController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $usuario = Usuario::findOrFail($id);
+        return view('usuarios.show', compact('usuario'));
     }
 
     /**
