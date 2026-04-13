18487094fdbbc39e26f18311e3a21f37463cfaa8
SindyEscobar <Sescobarc@unah.hn>
2026-04-12 23:27:28 -0600

    eliminar boton de plantilla del admin

--- resources/views/layouts/layoutadmin.blade.php
+++ resources/views/layouts/layoutadmin.blade.php
@@ -318,11 +318,6 @@
                class="btn btn-outline-light btn-sm px-3 rounded-pill shadow-sm">
                 <i class="fas fa-home me-1"></i> Inicio
             </a>
-            <a href="{{ route('admin.home.editor') }}"
-               class="btn btn-warning btn-sm px-3 rounded-pill shadow-sm">
-                <i class="fas fa-edit me-1"></i> Editar Página Principal
-            </a>
-
 
             @php
                 $adminNotiCount = \App\Models\Notificacion::where('usuario_id', auth()->id())
